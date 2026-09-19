9087fa70a98fcb6aeb3c6922e2cba2e9fa603210
mridul-123-maurya <mridulmaurya211@gmail.com>
2026-09-20 02:43:25 +0530

    some ui changes

---
+++ PrepAgentAi/frontend/src/components/LoginModel.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const LoginModel = ({onClose}) => {
+  return (
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/40 backdrop-blur-md px-4">
+
+        <div>
+            
+        </div>
+        
+    </div>
+  )
+}
+
+export default LoginModel
--- PrepAgentAi/frontend/src/pages/Home.jsx
+++ PrepAgentAi/frontend/src/pages/Home.jsx
@@ -1,11 +1,48 @@
-import React from 'react'
+import React from "react";
+import { motion } from "motion/react";
+import logo from "../assets/prepagent.svg";
+import { FaLongArrowAltRight } from "react-icons/fa";
 
 const Home = () => {
   return (
-    <div className='text-red-700 text-3xl'>
-      Home
-    </div>
-  )
-}
+    <div className="bg-white text-[#0A0A0A] font-sans min-h-screen overflow-x-hidden">
+      
+      <motion.nav
+        initial={{ y: -60, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.5, ease: "easeOut" }}
+        className="fixed top-0 left-0 right-0 z-50 h-[52px] flex items-center justify-between px-5 bg-white/70 backdrop-blur-xl border-b border-black/5"
+      >
+        <div className="flex items-center gap-2">
+          
+          <div className="w-7 h-7 rounded-lg bg-[#0A0A0A] flex items-center justify-center shadow-[0_4px_14px_rgba(0,0,0,0.18)]">
+            <img
+              src={logo}
+              alt="Logo"
+              className="w-5 h-5"
+            />
+          </div>
 
-export default Home
+          <span className="font-extrabold text-base tracking-tight text-[#0A0A0A]">
+            PrepAgentAi
+          </span>
+
+        </div>
+        <motion.button whileHover={{scale:1.04}} 
+        whileTap={{scale:0.97}}
+        className='bg-[#0A0A0A]/80 backdrop-blur-2xl text-white font-semibold border border-white/10 rounded-md px-3 py-2 text-xs cursor-pointer transition-all hover:border-white/20 shadow-[0_8px_24px_rgba(0,0,0,0.25)] flex items-center gap-2'>
+        Log In <FaLongArrowAltRight />
+
+        </motion.button>
+      </motion.nav>
+
+
+      <section className="relative pt-20 pb-14 overflow-hidden bg-[#F8F9FA]">
+
+      </section>
+
+    </div>
+  );
+};
+
+export default Home;
